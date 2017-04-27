160b4300f2205d29e96afbc022b033f58e4cb613
legshampoo <daniel.c.baker@gmail.com>
2017-04-27 11:24:57 -0400

    changed ip address

--- SolutionConstants.cs
+++ SolutionConstants.cs
@@ -8,29 +8,23 @@ namespace _45PPC_RFID
 {
     class SolutionConstants
     {
-        public const string ReaderHostname = "speedwayr-11-2C-3F.local";
-        public const float RxSensitivity = -80f;
+		//public const string ReaderHostname = "speedwayr-11-2C-3F.local";
+		public const string ReaderHostname = "192.168.45.80";
+        public const float RxSensitivity = -55f;
         public const float TxPower = 30.0f;
         public const float TagTimeout = 1.5f * 1000;
         public const int CheckReaderInterval = 10 * 1000;
-        public const string server = "192.168.32.39";
+		public const string server = "192.168.0.22"; //"192.168.32.39";
         public const int tcpPort = 5550;
 
         public static List<string> TagList = new List<string> {
-            "A020",
-            "0001",
-            "0002",
-            "1234",
-            "3008 33B2 DDD9 0140 0000 0000",
-            "0000 AD00 0013 0628 0111 3000",
-            "AD2B 0300 170A 1D7F 4C00 0054",
             "1000",
-            "AP20",
-            "AM00",
-            "N000",
-            "P100",
-            "P200",
-            "T000"
+            "2000",
+            "3000",
+            "4000",
+            "5000",
+            "6000",
+            "7000"
         };
     }
 }
